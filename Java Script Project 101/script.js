1f08a5f0117238a80caf3ddcda57ab135b48f58b
abrahampromise447 <54327158+abrahampromise447@users.noreply.github.com>
2019-08-20 18:12:48 +0100

    Add files via upload

---
+++ Java Script Project 101/script.js
@@ -0,0 +1,36 @@
+
+
+
+
+/*let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let value = ['Ace', 'King', 'Queen', 'Jack', 
+    'Ten', 'Nine', 'Eight', 'Seven', 'Six',
+    'Five', 'Four', 'Three', 'Two'];
+
+let deck = [];
+
+for (let suitsIdx = 0; suitsIdx < suits.length; suitsIdx++) {
+    for (let valueIdx = 0; valueIdx < value.length; valueIdx++) {
+        deck.push(value[valueIdx] + ' of ' + suits[suitsIdx] );
+    }
+}
+
+for (let i = 0; i < deck.length; i++) {
+    console.log(deck[i]);
+}
+
+let playerCards = [ deck[0], deck[2] ];
+    
+console.log("Welcome to Blackjack!");
+
+console.log("You are dealt: ");
+console.log("  " + playerCards[0]);
+console.log("  " + playerCards[1]);
+*/
+console.log("he is thruth");
+
+function myFunction(wee){
+  console.log(wee);
+}
+
+myFunction("this is it");
